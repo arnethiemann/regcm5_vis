2d56aeec2e20e842e71f61fac8804075e7a3d3ce
Arne <arne.thiemann@posteo.de>
2025-02-13 11:43:32 +0100

    Resampling model output for validation with GPCC; however the result seems weird

--- model_validation.R
+++ model_validation.R
@@ -9,7 +9,41 @@ library(terra)
 #---- GPCC data ----
 gpcc <- rast(
   "../observation_data/gpcc_first_guess_daily/first_guess_daily_202404.nc",
-  subds = "p"
+  subds = "p" # subset: p = precipitation
 )
 
+crs(gpcc) %>% writeLines()
+
+
+
+# import precipitation model output
+source("read_model_output.R")
+
+crs(regcm5_pr) %>% writeLines()
+
+
+# create daily sums of precipitation from model output
+prec_rcm_daily <- tapp(regcm5_pr, "days", fun = "mean")
+
+# sum up precipitation to mm/day
+prec_rcm_daily <- prec_rcm_daily * 86400
+
+# crop to bbox
+#uae_vec <- vect("../geodata/uae_admin.gpkg") %>% 
+#  terra::project(., gpcc)
+
+aoi <- terra::ext(48,60,19,30)
+gpcc_crop <- terra::crop(gpcc, aoi, touches = T)
+
+
+# project and crop model output
+prec_rcm_daily_repr <- terra::resample(prec_rcm_daily, gpcc_crop, "cubic")
+plot(prec_rcm_daily_repr)
+
+dim(gpcc_crop) == dim(prec_rcm_daily_repr)
+
+
+
+# Find overlapping dates
+common_timespan <- intersect(time(prec_rcm_daily_repr), time(gpcc_crop))
 
